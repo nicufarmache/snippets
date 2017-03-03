a0eaf2641861816e65895e23df1fb2b67c67fd92
Nicu <nicufarmache@users.noreply.github.com>
2017-03-03 09:50:03 +0200

    Created login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  loggedIn = false;
+  showAccount = false;
+  userDisplayName = "";
+  userPhotoURL = "";
+  uid = null;
+
+  items: FirebaseListObservable<any[]>;
+  
+  constructor(public af: AngularFire) {
+    af.auth.subscribe(auth => {
+      if (!auth){
+        this.loggedIn = false;
+        this.showAccount = true;
+        return;
+      }
+
+      this.loggedIn = true;
+      this.userDisplayName = auth.auth.displayName;
+      this.userPhotoURL = auth.auth.photoURL
+      this.showAccount = true;
+      this.uid = auth.uid;
+      this.items = af.database.list('/users/' + this.uid);
+    });
+  }  
+  login() {
+    this.af.auth.login();
+  }
+  logout() {
+     this.af.auth.logout();
+  }
+
+  newSnippet() {
+    this.items.push({name:"test", description:"alalalala"});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ng2-bootstrap';
+import { ButtonsModule } from 'ng2-bootstrap/buttons';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA8I7irjtAshOVqMPlqd66UZ-DRp7UKGeM",
+  authDomain: "snippets-ddd8d.firebaseapp.com",
+  databaseURL: "https://snippets-ddd8d.firebaseio.com",
+  storageBucket: "snippets-ddd8d.appspot.com",
+  messagingSenderId: "538785453132"
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+};
 
 @NgModule({
   declarations: [
@@ -11,6 +27,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
+    AlertModule.forRoot(),
+    ButtonsModule.forRoot(),
     FormsModule,
     HttpModule
   ],
